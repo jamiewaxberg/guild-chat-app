6fa40e1370e5a9aeb1f5a9644b23774c18cc3cda
Jamie <jamie@Benjis-MacBook-Pro.local>
2020-02-05 10:34:50 -0700

    modifying readme, publishing to github pages

---
+++ build/precache-manifest.f6a9e986c3e35d345a72eaae06cf2426.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b75ec8bc1cd28f6f1df6310724ba764",
+    "url": "/guild-chat-app/index.html"
+  },
+  {
+    "revision": "4f2f7b23bb14f2ec994c",
+    "url": "/guild-chat-app/static/css/main.36ea8610.chunk.css"
+  },
+  {
+    "revision": "017f2dcfe635bb29434a",
+    "url": "/guild-chat-app/static/js/2.681099fb.chunk.js"
+  },
+  {
+    "revision": "ab427a65ed80b935d451c180b71c0220",
+    "url": "/guild-chat-app/static/js/2.681099fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f2f7b23bb14f2ec994c",
+    "url": "/guild-chat-app/static/js/main.0b7c67ab.chunk.js"
+  },
+  {
+    "revision": "95c7e7712550c9591dfc",
+    "url": "/guild-chat-app/static/js/runtime-main.fa066239.js"
+  }
+]);
